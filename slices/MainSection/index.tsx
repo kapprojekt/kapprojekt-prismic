e2408d36f427811ec4aea90ac468f50247b8a20c
kysniw <kuba.winsyk.forgames@gmail.com>
2026-02-19 20:01:17 +0100

    Created first slice

---
+++ slices/MainSection/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+
+export type HomeSectionProps = SliceComponentProps<Content.HomeSectionSlice>;
+
+const HomeSection: FC<HomeSectionProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <h1>{slice.primary.title}</h1>
+      <h3>{slice.primary.subtitle}</h3>
+      <PrismicRichText field={slice.primary.text} />
+      {slice.primary.buttonlink.map((link) => (
+        <PrismicNextLink key={link.key} field={link} className={link.variant} />
+      ))}
+      {slice.primary.socialmedialink.map((link) => (
+        <PrismicNextLink key={link.key} field={link}>
+          Link
+        </PrismicNextLink>
+      ))}
+      <PrismicNextImage field={slice.primary.background} />
+    </section>
+  );
+};
+
+export default HomeSection;
